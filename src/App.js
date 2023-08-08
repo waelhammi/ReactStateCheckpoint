c355c0cfad1642ac61634ecc7328f1eff20f6c99
waelhammi <weal.hammi@gmail.com>
2023-08-08 09:21:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Profile from './Profile/Profile';
+import './App.css';
+class App extends React.Component {
+  // =============================Constractor=============================
+  constructor(props){
+    super(props);
+    this.state={
+      Person :{ 
+                fullName:   'Wael Hammi',
+                bio:        'Full stack web development, I am a fast learner and always love to develop myself . ',
+                imgSrc:     '/assets/avatar.png',
+                professio:  'Full stack developer'
+              },
+      show:false ,
+      count: 0
+              };
+  }
+  
+  // =============================toggle function=============================
+  toggle =() =>{this.setState({show:!this.state.show});}
+
+  // ========================increment Counter function========================
+  increment = () => {this.state.show ? this.setState({ count: this.state.count + 1 }) : this.setState({ count: 0 });};
+
+  // ========================componentDidMount lifecyle========================
+  componentDidMount = () => {setInterval(this.increment, 1000);}
+
+  render(){
+    return (
+    <div className="App">
+      {/* counter */}
+      <h4 style={{ color: '#457b9d' }}>
+        {this.state.count} seconds .
+      </h4>
+      {/* show button */}
+      <button className='btnShow' onClick={this.toggle}>Show Profile</button>
+      {this.state.show && <Profile Person={this.state.Person}/>}
+    </div>
+  );
+  }
+}
+export default App;
